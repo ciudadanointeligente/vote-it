6141c7c1c8847112730c4ba80b5da36a50b4c17e
José Damián Garrido Muñoz <damian.garrido.munoz@gmail.com>
2014-11-23 16:20:35 -0300

    improve visualizations on main page

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,9 @@
 class VisitorsController < ApplicationController
+  def index
+    @votes_count = Hash.new
+    @last_votes = Vote.all.order(date: :desc).limit(2)
+    @last_votes.each do |v|
+      @votes_count[v.id] = VoteEvent.where(vote_id: v.id).group("option").count
+    end
+  end
 end
