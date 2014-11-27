2d3aec8b9b9b4769f2aef515dc53ce9cc8c85ec9
José Damián Garrido Muñoz <damian.garrido.munoz@gmail.com>
2014-11-27 13:18:04 -0300

    improve viz
    add styles
    refact code

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,8 +2,5 @@ class VisitorsController < ApplicationController
   def index
     @votes_count = Hash.new
     @last_votes = Vote.all.order(date: :desc).limit(2)
-    @last_votes.each do |v|
-      @votes_count[v.id] = VoteEvent.where(vote_id: v.id).group("option").count
-    end
   end
 end
