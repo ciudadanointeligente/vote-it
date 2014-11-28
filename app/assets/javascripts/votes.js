6ff4dcd26f19f63c6f62c5603016e95a536ff672
José Damián Garrido Muñoz <damian.garrido.munoz@gmail.com>
2014-11-28 18:08:34 -0300

    fix a bug

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -1,29 +1,31 @@
 (function(){
-  var bills = document.querySelector('#bills'),
-      parliamentarians = document.querySelector('#parliamentarians'),
-      party = document.querySelector('#party');
-    
-  bills.addEventListener('mouseover', function(){
-    bills.classList.add('halo');
-  })
+  if( document.querySelector('#bills') ) {
+    var bills = document.querySelector('#bills'),
+        parliamentarians = document.querySelector('#parliamentarians'),
+        party = document.querySelector('#party');
+      
+    bills.addEventListener('mouseover', function(){
+      bills.classList.add('halo');
+    })
 
-  bills.addEventListener('mouseout', function(){
-    bills.classList.remove('halo');
-  })
+    bills.addEventListener('mouseout', function(){
+      bills.classList.remove('halo');
+    })
 
-  parliamentarians.addEventListener('mouseover', function(){
-    parliamentarians.classList.add('halo');
-  })
+    parliamentarians.addEventListener('mouseover', function(){
+      parliamentarians.classList.add('halo');
+    })
 
-  parliamentarians.addEventListener('mouseout', function(){
-    parliamentarians.classList.remove('halo');
-  })
+    parliamentarians.addEventListener('mouseout', function(){
+      parliamentarians.classList.remove('halo');
+    })
 
-  party.addEventListener('mouseover', function(){
-    party.classList.add('halo');
-  })
+    party.addEventListener('mouseover', function(){
+      party.classList.add('halo');
+    })
 
-  party.addEventListener('mouseout', function(){
-    party.classList.remove('halo');
-  })
+    party.addEventListener('mouseout', function(){
+      party.classList.remove('halo');
+    })
+  }
 })();
