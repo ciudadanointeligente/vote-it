b78869cbb4f7b04af5d18f148ff658448751e501
José Damián Garrido Muñoz <damian.garrido.munoz@gmail.com>
2014-12-02 16:38:59 -0300

    small changes in personal info

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def get_party_name id
+    orgs = JSON.parse( open('app/assets/json/organizations.json').read )['result']
+    orgs.select {|org| org['id']== id}.first['name']
+  end
 end
