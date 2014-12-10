7b6b3e249b8aab6d25828f208dd3bd2542133dd4
José Damián Garrido Muñoz <damian.garrido.munoz@gmail.com>
2014-12-10 16:55:58 -0300

    add visualization of low chamber

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,6 @@
 class VisitorsController < ApplicationController
   def index
     @votes_count = Hash.new
-    @last_votes = Vote.all.order(date: :desc).limit(2)
+    @last_votes = Vote.all.order(date: :desc)
   end
 end
